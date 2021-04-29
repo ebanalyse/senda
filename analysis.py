f4b998594d3ce23c2a2bbb80472127323c611c7d
EC2 Default User <ec2-user@ip-172-31-11-179.eu-west-1.compute.internal>
2021-04-29 11:05:30 +0000

    add analysis

---
+++ analysis.py
@@ -0,0 +1,111 @@
+from nltk.tokenize import sent_tokenize
+import pandas as pd
+from senda import Model
+import numpy as np
+from senda import Model
+import seaborn as sns
+import matplotlib as plt
+m = Model(transformer = "pin/senda")
+m.init()
+#from SENDA import get_danish_tweets
+#train, eval, test = get_danish_tweets()
+import pandas as pd
+
+section = "Alle"
+n = 5000
+if section == "Alle":
+    section_select = None
+else:
+    section_select = section
+
+from ebpaperboy.news import get_news
+newspaper = get_news(publication = "ekstrabladet",
+                     n_limit = n,
+                     order_by = "first_published",
+                     section_name = section_select)
+
+# helper function to convert numpy types to native python types (to avoid app crashing).
+def convert_native_python(x):
+    return getattr(x, "tolist", lambda: x)()
+
+# function to compute aggregated sentiment scores on article level.
+def predict_article(text):
+
+    sents = sent_tokenize(text)
+    data = {'text': sents, 'label': [m.labels[0]]*len(sents)}
+    data = pd.DataFrame(data)
+
+    preds = m.predict(data).predictions
+    
+    # compute predicted labels.
+    preds = np.argmax(preds, -1)
+    
+    #### MEAN SENTIMENT SCORE####
+    # compute mean sentiment "score" across sentences.
+    mean = convert_native_python(preds.mean() - 1)
+
+    #### Ratio between positive and negative sentences ####
+    count_pos = sum(preds == 2)
+    count_neg = sum(preds == 0)
+    count_neutral = sum(preds == 1)
+    # add 1 to avoid division with zero.
+    ratio_pos_neg = (count_pos + 1) / (count_neg + 1)
+    ratio_pos_neg = convert_native_python(ratio_pos_neg)
+
+    return {'mean': [mean], 
+            'ratio_pos_neg': [ratio_pos_neg],
+            'ratio_loaded': [(count_pos+count_neg)/len(sents)],
+            'n_positiv': [count_pos],
+            'n_negativ': [count_neg],
+            'n_neutral': [count_neutral]}
+
+#def sent(x):
+#
+#    x = x.get_clean()
+#    return predict_article(x)
+
+import time
+
+start = time.time()
+preds = []
+for i, x in enumerate(newspaper.news):
+    try:
+        x = newspaper.news[i].get_clean()
+        res = predict_article(x)
+        res = pd.DataFrame.from_dict(res)
+        preds.append(res)
+    except: 
+        pass
+# results = [sent(x) for x in newspaper.news]
+end = time.time()
+print(end - start)
+preds = pd.concat(preds)
+preds.to_csv(f'{section}.csv', index = False)
+
+# preds = pd.read_csv("....csv")
+out = sns.histplot(data=preds, x="mean", kde = True, bins = 30)
+out.set(title=f'Sektion: {section}',xlabel='Polarity(mean)', ylabel='Count')
+out.get_figure().savefig(f'{section}.png')
+out.get_figure().clf()
+
+# import numpy as np
+var = preds["mean"]
+
+from scipy.stats import kurtosis, skew
+import numpy as np
+import math
+percentiles = np.percentile(var, [0,10,20,30,40,50,60,70,80,90,100]).tolist()
+percentiles = [round(x, 2) for x in percentiles]
+statistics = {
+    "Mean": [round(np.mean(var), 2)],
+    "Median": [round(np.median(var), 2)],
+    "Standard Deviation": [round(np.std(var), 2)],
+    "Skewness": [round(skew(var), 2)],
+    "Kurtosis": [round(kurtosis(var), 2)],
+    #"Deciles": percentiles
+}
+pd.DataFrame.from_dict(statistics, orient = "index")
+
+
+
+
