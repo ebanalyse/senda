461431271802ab9e8994944eac2b87deef78bcfd
EC2 Default User <ec2-user@ip-172-31-11-179.eu-west-1.compute.internal>
2021-05-03 13:06:30 +0000

    improve analysis

--- analysis.py
+++ analysis.py
@@ -11,19 +11,6 @@ m.init()
 #train, eval, test = get_danish_tweets()
 import pandas as pd
 
-section = "Alle"
-n = 5000
-if section == "Alle":
-    section_select = None
-else:
-    section_select = section
-
-from ebpaperboy.news import get_news
-newspaper = get_news(publication = "ekstrabladet",
-                     n_limit = n,
-                     order_by = "first_published",
-                     section_name = section_select)
-
 # helper function to convert numpy types to native python types (to avoid app crashing).
 def convert_native_python(x):
     return getattr(x, "tolist", lambda: x)()
@@ -43,7 +30,23 @@ def predict_article(text):
     #### MEAN SENTIMENT SCORE####
     # compute mean sentiment "score" across sentences.
     mean = convert_native_python(preds.mean() - 1)
+    
+    # compute article label
+    if mean > 0.1:
+        label_article = "positiv"
+    elif mean < -0.15:
+        label_article = "negativ"
+    else:
+        label_article = "neutral"
 
+    # compute header label
+    if preds[0] == 0:
+        label_header = "negativ"
+    elif preds[0] == 1:
+        label_header = "neutral"
+    else:
+        label_header = "positiv"
+     
     #### Ratio between positive and negative sentences ####
     count_pos = sum(preds == 2)
     count_neg = sum(preds == 0)
@@ -52,60 +55,80 @@ def predict_article(text):
     ratio_pos_neg = (count_pos + 1) / (count_neg + 1)
     ratio_pos_neg = convert_native_python(ratio_pos_neg)
 
-    return {'mean': [mean], 
+    return {'label_article': [label_article],
+            'label_header': [label_header],
+            'mean': [mean], 
             'ratio_pos_neg': [ratio_pos_neg],
             'ratio_loaded': [(count_pos+count_neg)/len(sents)],
             'n_positiv': [count_pos],
             'n_negativ': [count_neg],
             'n_neutral': [count_neutral]}
 
-#def sent(x):
-#
-#    x = x.get_clean()
-#    return predict_article(x)
+def compute_statics(section = "Sport", n=500):
 
-import time
+    if section == "Alle":
+        section_select = None
+    else:
+        section_select = section
 
-start = time.time()
-preds = []
-for i, x in enumerate(newspaper.news):
-    try:
-        x = newspaper.news[i].get_clean()
-        res = predict_article(x)
-        res = pd.DataFrame.from_dict(res)
-        preds.append(res)
-    except: 
-        pass
-# results = [sent(x) for x in newspaper.news]
-end = time.time()
-print(end - start)
-preds = pd.concat(preds)
-preds.to_csv(f'{section}.csv', index = False)
+    from ebpaperboy.news import get_news
+    newspaper = get_news(publication = "ekstrabladet",
+                         n_limit = n,
+                         order_by = "first_published",
+                         section_name = section_select)
+    import time
 
-# preds = pd.read_csv("....csv")
-out = sns.histplot(data=preds, x="mean", kde = True, bins = 30)
-out.set(title=f'Sektion: {section}',xlabel='Polarity(mean)', ylabel='Count')
-out.get_figure().savefig(f'{section}.png')
-out.get_figure().clf()
+    start = time.time()
+    preds = []
+    for i, x in enumerate(newspaper.news):
+        try:
+            x = newspaper.news[i].get_clean()
+            res = predict_article(x)
+            res = pd.DataFrame.from_dict(res)
+            preds.append(res)
+        except: 
+            pass
+    # results = [sent(x) for x in newspaper.news]
+    end = time.time()
+    print(end - start)
+    preds = pd.concat(preds)
+    preds.to_csv(f'{section}.csv', index = False)
 
-# import numpy as np
-var = preds["mean"]
+    # preds = pd.read_csv("....csv")
+    out = sns.histplot(data=preds, x="mean", kde = True, bins = 30)
+    out.set(title=f'Sektion: {section}',xlabel='Polarity(mean)', ylabel='Count')
+    out.get_figure().savefig(f'{section}.png')
+    out.get_figure().clf()
 
-from scipy.stats import kurtosis, skew
-import numpy as np
-import math
-percentiles = np.percentile(var, [0,10,20,30,40,50,60,70,80,90,100]).tolist()
-percentiles = [round(x, 2) for x in percentiles]
-statistics = {
-    "Mean": [round(np.mean(var), 2)],
-    "Median": [round(np.median(var), 2)],
-    "Standard Deviation": [round(np.std(var), 2)],
-    "Skewness": [round(skew(var), 2)],
-    "Kurtosis": [round(kurtosis(var), 2)],
-    #"Deciles": percentiles
-}
-pd.DataFrame.from_dict(statistics, orient = "index")
+    # import numpy as np
+    var = preds["mean"]
 
+    from scipy.stats import kurtosis, skew
+    import numpy as np
+    import math
+    #percentiles = np.percentile(var, [0,10,20,30,40,50,60,70,80,90,100]).tolist()
+    #percentiles = [round(x, 2) for x in percentiles]
+    statistics = {
+        "Section": section,
+        "Mean": [round(np.mean(var), 2)],
+        "Median": [round(np.median(var), 2)],
+        "Standard Deviation": [round(np.std(var), 2)],
+        "Skewness": [round(skew(var), 2)],
+        "Kurtosis": [round(kurtosis(var), 2)],
+        #"Deciles": percentiles
+    }
+    pd.DataFrame.from_dict(statistics, orient = "index")
 
-
-
+    sections = ["Erhverv",
+                "Danske kendte",
+                "Videnskab",
+                "Politik",
+                "Danske kongelige",
+                "Sport",
+                "Dansk fodbold",
+                "Videnskab",
+                "Krimi",
+                "Samfund",
+                "Krimi",
+                "112",
+                "Teknologi"]
